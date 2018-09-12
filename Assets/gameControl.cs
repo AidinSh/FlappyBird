7e4406228097d82748a2b71b171de3e70119d247
AidinSh <aidin_shahmoradi@yahoo.com>
2018-09-12 18:11:43 +0430

    game over
    add game controller

---
+++ Assets/gameControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameControl : MonoBehaviour {
+
+    public GameObject GameOverText;
+    public bool gameOver = false;
+    public static gameControl instance;
+	
+	void Awake () {
+        if ( instance == null )
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy (gameObject);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if ( gameOver == true && Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }	
+	}
+    public void BirdDied()
+    {
+        GameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
